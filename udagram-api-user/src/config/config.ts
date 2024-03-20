5422547023474fffebc8140f1e72c177358629cd
leduy1561993 <leduy1561993@gmail.com>
2024-03-20 23:40:21 +0700

    add source

---
+++ udagram-api-user/src/config/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+  'username': 'postgres',
+  'password': 'duylk123456',
+  'database': 'postgres',
+  'host': 'cdr.cje2nmjuenj7.us-east-1.rds.amazonaws.com',
+  'dialect': 'postgres',
+  'aws_region': 'us-east-1',
+  'aws_profile': 'default',
+  'aws_media_bucket': 'pro3-196343632393',
+  'url': 'http://localhost:8100',
+  'jwt': {
+    'secret': 'testing',
+  },
+};
