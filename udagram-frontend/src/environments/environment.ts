b2469df17591ec4b2043af24be21c3be7f00c896
leduy1561993 <leduy1561993@gmail.com>
2024-04-08 23:15:16 +0700

    refactor source

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a5a362791bda747a6b5360ebb1657e27-278189001.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
